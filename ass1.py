0391935487023a6927c9b8a47cd6fa5ce6a94b8d
ChuHongHeMike <132968805+ChuHongHeMike@users.noreply.github.com>
2023-09-26 01:57:03 -0400

    edit the format

--- ass1.py
+++ ass1.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import pandas as pd
+import os
 
+file_path = 'output.csv'
 # get the htlm
 resp = requests.get('https://ca.trustpilot.com/review/equitablebank.ca')
-
-print(resp.text)
 # This is the html source
 html_code = resp.text
 
@@ -26,55 +26,58 @@ review_count = 0
 current = 'https://ca.trustpilot.com/review/equitablebank.ca'
 page = 'https://ca.trustpilot.com'
 All_page=[current]
-while current:
-    response = requests.get(current)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        next_page = soup.find_all(name="a", string='Next page')
-        review = soup.find_all(
-            name='div',
-            attrs={
-              'class':'styles_cardWrapper__LcCPA styles_show__HUXRb styles_reviewCard__9HxJJ'
-            }
-        )
-        for section in review:
-            para = section.find(
+name = 'aa'
+with open(file_path, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',')
+    csv_writer.writerow(["Name", "Time", "Rating", "Text"])
+    while current:
+        response = requests.get(current)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            next_page = soup.find_all(name="a", string='Next page')
+            review = soup.find_all(
+               name='div',
+               attrs={
+                 'class':'styles_cardWrapper__LcCPA styles_show__HUXRb styles_reviewCard__9HxJJ'
+               }
+            )
+            for section in review:
+                para = section.find(
                               name = 'p',
                               class_ = "typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn"
-        )
-            time_tag = section.find(
-                  name = 'time'
-            )
-            ratingvalue = section.find(
-                  class_="styles_reviewHeader__iU9Px"
-            )
-            if review_count >= 10:
-                current = False
-                break
-            elif para:
-                paras = para.text
-                data_time.append(time_tag['datetime'])
-                data_para.append(paras)
-                data_ratingvalue.append(ratingvalue['data-service-review-rating'])
-                review_count += 1
-            else:
-                paras = None
-                data_time.append(time_tag['datetime'])
-                data_para.append(paras)
-                data_ratingvalue.append(ratingvalue['data-service-review-rating'])
-                review_count += 1
+                )
+                time_tag = section.find(
+                    name = 'time'
+                )
+                ratingvalue = section.find(
+                    class_="styles_reviewHeader__iU9Px"
+                )
+                if review_count >= 10:
+                    current = False
+                    break
+                elif para:
+                    paras = para.text
+                    data_time.append(time_tag['datetime'])
+                    data_para.append(paras)
+                    data_ratingvalue.append(ratingvalue['data-service-review-rating'])
+                    csv_writer.writerow([name, time_tag['datetime'], ratingvalue['data-service-review-rating'], paras])
+                    review_count += 1
+                else:
+                    paras = None
+                    data_time.append(time_tag['datetime'])
+                    data_para.append(paras)
+                    data_ratingvalue.append(ratingvalue['data-service-review-rating'])
+                    csv_writer.writerow([name, time_tag['datetime'], ratingvalue['data-service-review-rating'], paras])
+                    review_count += 1
 
-        if next_page[0].get('href', ''):
-            next_url = page + next_page[0]['href']
-            current = next_url
-            print(current)
+            if next_page[0].get('href', ''):
+                next_url = page + next_page[0]['href']
+                current = next_url
+                print(current)
+            else:
+                current = False
         else:
             current = False
-    else:
-        current = False
 
 
-
-df = pd.DataFrame({'Text': data_para, 'Time':data_time, 'Rating':data_ratingvalue, 'Name': 'Equitable Bank'})
-
-df
+csv_file.close()
